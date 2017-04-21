b56a9d298825ce76648e02b912addb1b72567e2a
dispatch-pages <1071516822@qq.com>
2017-04-21 23:11:19 +0800

    Update generic-digit-function.lisp

--- generic-digit-function.lisp
+++ generic-digit-function.lisp
@@ -58,3 +58,88 @@
 						(elt string j)))
 		(setf tag nil)
 		(return)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun threat (i j a b)                   ;;八皇后问题 
+  (or
+   (equal i a)
+   (equal j b)
+   (equal (- i j) (- a b))
+   (equal (+ i j) (+ a b))))              ;对于两个皇后所在的位置是否相互威胁的判断
+	 
+(defun eight-queens-problem(queens-size)
+  (queens-aux nil 0 queens-size))
+
+(defun queens-aux (board n size)
+  (cond
+	((= n size)
+	 (print (reverse board)))
+	(t
+	 (queens-sub board n 0 size))))
+(defun queens-sub (board n m size)
+  (cond
+	(( = m size))
+	(t
+	 (cond
+	  ((conflict n m board))    ;注意，该表达式，并非多余，很巧秒的地方
+	   (t
+		(queens-aux (cons (list n m)
+						  board)
+					(1+ n)
+					size)))
+	 (queens-sub board n (+ m 1) size))))
+	 
+(defun conflict (n m board)                           ;对于任意两个皇后是否冲突的判断，使用递归方法
+  (cond
+	(( null board) nil)
+	((or
+	  (threat n m (caar board) (cadar board))
+	  (conflict n m (cdr board))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defun searchb (start finish)
+  (searcha (list start ) finish))
+(defun searcha (queue finish)
+  (cond
+	((null queue) nil)
+	((equal finish (car queue)) t)
+	(t
+	 (searcha (expand (car queue))
+			  finish))))
+(defun expand (path )
+  (mapcar #'(lambda (children) (cons children path))
+		  (get (car path) 'children)))
+(defun depth(start finish)                           ;深度优先搜索
+  (deptha (list(list start)) finish))
+(defun deptha (queue finish)
+  (cond
+	((null queue) nil)
+	((equal finish (caar queue))
+	 (reverse (car queue)))
+	(t
+	 (deptha
+	  (append(expand (car queue))
+			 (cdr queue))
+	  finish))))
+(defun set-children()
+  (setf (get 's 'children) '(l o)
+		(get 'l 'children) '(m f)
+		(get 'm 'children) '(n)
+		(get 'n 'children) '(f)
+		(get 'o 'children) '(p q)
+		(get 'p 'children) '(f)
+		(get 'q 'children) '(f)))
+(defun breadth(start finish)                           ;广度优先搜索
+  (breadth-a (list (list start )) finish))
+
+(defun breadth-a (queue finish )                     
+  (cond
+	((null queue ) nil)
+	((equal (caar queue)finish)
+	 (reverse (car queue)))
+	(t
+	 (breadth-a (append (cdr queue)
+					  (expand (car queue)))
+			  finish))))
